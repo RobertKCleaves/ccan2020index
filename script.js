13ddc8c0efdd02b46bbb5b3baa76bb60a36f16f3
Robbie Cleaves <robbiecleaves@gmail.com>
2020-11-18 17:32:32 -0600

    made accordion.js it's own file, split h3's into individual h4s, italized titles

--- script.js
+++ script.js
@@ -16,27 +16,33 @@ function showArt(obj) {
 
   for(let i = 0; i < art_piece.length; i++) {
     const headLine = document.createElement('h3');
+    const theWinner = document.createElement('h4');
+    const theNumber = document.createElement('a');
+    const theTitle = document.createElement('i');
+    const theArtist = document.createElement('h4');
     const theInfo = document.createElement('ul');
-    const theCity = document.createElement('li')
-    const theMedium = document.createElement('li')
-    const thePrice = document.createElement('li')
+    const theCity = document.createElement('li');
+    const theMedium = document.createElement('li');
+    const thePrice = document.createElement('li');
     const line = document.createElement('hr');
 
-    headLine.textContent =
-      art_piece[i].winner + ' ' +
-      art_piece[i].number + ' ' +
-      art_piece[i].title + ' - ' +
-      art_piece[i].artist;
+
+    theWinner.textContent = art_piece[i].winner + ' ';
+    theNumber.textContent = art_piece[i].number + ' ';
+    theTitle.textContent = art_piece[i].title;
+    theArtist.textContent = ' - ' + art_piece[i].artist;
     theCity.textContent = 'City: ' + art_piece[i].city;
     theMedium.textContent = 'Medium: ' + art_piece[i].medium;
     thePrice.textContent = 'Price: $' + art_piece[i].price;
 
+    headLine.appendChild(theWinner);
+    headLine.appendChild(theNumber);
+    headLine.appendChild(theTitle);
+    headLine.appendChild(theArtist);
     theInfo.appendChild(theCity);
     theInfo.appendChild(theMedium);
     theInfo.appendChild(thePrice);
     section.appendChild(headLine);
     section.appendChild(theInfo);
-
-
   }
 }
