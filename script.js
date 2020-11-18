e747fb6712b021319fbcb99ca77f2500534afc83
Robbie Cleaves <robbiecleaves@gmail.com>
2020-11-18 15:21:17 -0600

    implement jQuery accordion class, remade info p's into li's accordingly

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const section = document.querySelector('section');
-const aside = document.querySelector('aside');
 
 let requestURL = 'ccan2020index.json';
 let request = new XMLHttpRequest();
@@ -16,33 +15,28 @@ function showArt(obj) {
   const art_piece = obj['pieces'];
 
   for(let i = 0; i < art_piece.length; i++) {
-    const myArticle = document.createElement('article');
-    const myInfo = document.createElement('p');
-    const myH3 = document.createElement('h3');
-    const myPara1 = document.createElement('p');
-    const myPara2 = document.createElement('p');
-    const myPara3 = document.createElement('p');
+    const headLine = document.createElement('h3');
+    const theInfo = document.createElement('ul');
+    const theCity = document.createElement('li')
+    const theMedium = document.createElement('li')
+    const thePrice = document.createElement('li')
     const line = document.createElement('hr');
 
-  myH3.textContent =
-    art_piece[i].winner + ' ' +
-    art_piece[i].number + ' ' +
-    art_piece[i].title + ' - ' +
-    art_piece[i].artist;
-  myPara1.textContent = 'City: ' + art_piece[i].city;
-  myPara2.textContent = 'Medium: ' + art_piece[i].medium;
-  myPara3.textContent = 'Price: $' + art_piece[i].price;
+    headLine.textContent =
+      art_piece[i].winner + ' ' +
+      art_piece[i].number + ' ' +
+      art_piece[i].title + ' - ' +
+      art_piece[i].artist;
+    theCity.textContent = 'City: ' + art_piece[i].city;
+    theMedium.textContent = 'Medium: ' + art_piece[i].medium;
+    thePrice.textContent = 'Price: $' + art_piece[i].price;
 
+    theInfo.appendChild(theCity);
+    theInfo.appendChild(theMedium);
+    theInfo.appendChild(thePrice);
+    section.appendChild(headLine);
+    section.appendChild(theInfo);
 
-    myArticle.appendChild(myH3);
-    myInfo.appendChild(myPara1);
-    myInfo.appendChild(myPara2);
-    myInfo.appendChild(myPara3);
-    myArticle.appendChild(line);
-
-    section.appendChild(myArticle);
-    section.appendChild(myInfo);
-    section.appendChild(line);
 
   }
 }
