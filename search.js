617c85462b4b85be773ba221d2470abfe1ca4a11
Robbie Cleaves <robbiecleaves@gmail.com>
2020-11-20 15:37:10 -0600

    cleaned up code, removed search.js and search bar/sticky footer

--- search.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("#searchbar").on("keyup", function() {
-    console.log('hello?')
-    var value = $(this).val().toLowerCase();
-    $("h3").filter(function() {
-      $("a").toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
